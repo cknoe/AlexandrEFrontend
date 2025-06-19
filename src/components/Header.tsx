409fb030eed791d5445ce9a9094a359cfb47d311
ck <none>
2025-06-19 03:25:32 +0200

    position fixed header + remove container css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,11 @@
 export default function Header() {
 return (
+    <>
     <header className="header">
         <span>Hello Header</span>
     </header>
+    <div className="blankspace">
+    </div>
+    </>
 );
 }
