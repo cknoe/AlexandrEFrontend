98314523070aac2d0b4233eecd2cfa3c70dff0e1
ck <none>
2025-06-24 05:16:55 +0200

    route error

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom'
 import App from './App'
+import Error from './components/Error'
 import LoginForm from './components/LoginForm'
 
 export default function AppRoutes() {
@@ -7,6 +8,7 @@ export default function AppRoutes() {
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/login" element={<LoginForm />} />
+      <Route path="*" element={<Error />} />
     </Routes>
   )
 }
