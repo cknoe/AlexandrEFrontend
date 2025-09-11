3d3d346a622c15b9a794a3375015fb0d913d7749
ck <none>
2025-09-11 07:08:47 +0200

    Revert "not using logodev api if response 202"
    This reverts commit cf19d5c508de60e86bd9ede6f886b4c5d93461a9.

--- src/utils/logoCache.ts
+++ src/utils/logoCache.ts
@@ -1,5 +1,3 @@
-import link from "../assets/link.png";
-
 const LOGO_DEV_KEY = import.meta.env.VITE_LOGO_DEV_KEY
 
 export function getCachedLogo(domain: string): string | null {
@@ -15,10 +13,7 @@ export async function fetchLogo(domain: string): Promise<string> {
   const cached = getCachedLogo(domain);
   if (cached) return cached;
 
-  let logoUrl = `https://img.logo.dev/${domain}?token=${LOGO_DEV_KEY}`;
-  const data = await fetch(logoUrl)
-  if (data.status == 202)
-    logoUrl= link
+  const logoUrl = `https://img.logo.dev/${domain}?token=${LOGO_DEV_KEY}`;
   setCachedLogo(domain, logoUrl);
   return logoUrl;
 }
