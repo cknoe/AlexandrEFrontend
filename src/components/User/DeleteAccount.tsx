35d43e351c91c451a2ca41e1cb81a929e3e91653
ck <none>
2025-09-15 03:37:28 +0200

    alert on account deletion

--- src/components/User/DeleteAccount.tsx
+++ src/components/User/DeleteAccount.tsx
@@ -9,9 +9,12 @@ export default function DeleteAccount() {
   const { closeModal } = useModal();
 
   async function handleClick() {
-    await apiDeleteUser()
-    logout() 
-    closeModal()
+    const isConfirmed: boolean = confirm("You are about to delete your account, are you sure ? You won't be able to retrieve it after this.")
+    if (isConfirmed) {
+      await apiDeleteUser()
+      logout() 
+      closeModal()
+    }
   }
 
   return (
