8b71cf97a8594a5a12303cd3aa694c1349250b0e
ck <none>
2025-09-20 17:26:23 +0200

    collection add feature

--- src/api/collection.ts
+++ src/api/collection.ts
@@ -27,6 +27,15 @@ export async function getCollectionById(id: number): Promise<Card[]> {
   return apiCards.map(apiToCard)
 }
 
+export async function createCollection(collectionName: String): Promise<Collection> {
+  const response = apiFecth('/collections', {
+    method: 'POST',
+    body: '{"name": "' + collectionName + '"}',
+  })
+  return apiToCollection(await response)
+}
+
+
 function apiToCollection(apiCollection: ApiCollection): Collection {
   return {
     collectionId: apiCollection.id,
