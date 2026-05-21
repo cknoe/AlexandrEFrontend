97c5234fdbffa222b0051d212f7cdde7023a1886
ck <none>
2026-05-21 23:16:23 +0200

    color + name changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -28,7 +28,7 @@ export default function Header() {
     <>
       <header className="header">
         <div className="header-left">
-          <h2 className="header-logo">hello</h2>
+          <h2 className="header-logo">Alexandr-e</h2>
         </div>
         <div className="header-center">
           <h2 className="header-title">{title}</h2>
